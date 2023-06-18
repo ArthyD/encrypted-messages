06f283c57db06844aa3b7c7edf1b884e0d7a4779
ArthurD <arthur.docquois@wanadoo.fr>
2023-06-18 12:51:55 +0200

    Classe chiffrement

---
+++ Client/cipher.py
@@ -0,0 +1,100 @@
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import AES,PKCS1_OAEP
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Signature import pkcs1_15
+from Cryptodome.Hash import SHA256
+from io import BytesIO
+import base64
+import zlib
+
+class Cryptor:
+    def __init__(self,public_key,private_key,other_pub_key):
+        self.public_key = public_key
+        self.private_key = private_key
+        self.other_pub_key = other_pub_key
+
+    def generate_keys(self):
+        new_key = RSA.generate(2048)
+        self.private_key = new_key.exportKey()
+        self.public_key = new_key.publickey().exportKey()
+
+    def save_priv_key(self,path):
+        with open(path+'/key.pri','wb') as f:
+            f.write(self.private_key)
+
+    def save_pub_key(self, path):
+        with open(path+'/key.pub','wb') as f:
+            f.write(self.public_key)
+
+    def load_keys(self, path):
+        with open(path+'/key.pri') as f:
+            self.private_key = f.read()
+        with open(path+'/key.pub') as f:
+            self.public_key = f.read()
+
+    def set_other_pub_key(self,other_pub_key):
+        self.other_pub_key = other_pub_key
+
+    def get_rsa_cipher(self,keytype):
+        if keytype == 'private':
+            rsakey = RSA.importKey(self.private_key)
+        elif keytype == 'public':
+            rsakey = RSA.importKey(self.public_key)
+        elif keytype == 'other':
+            rsakey = RSA.importKey(self.other_pub_key)
+        else:
+            raise Exception("Unknown type of key")
+        return (PKCS1_OAEP.new(rsakey), rsakey.size_in_bytes())
+    
+    def encrypt_message(self, plaintext):
+        compressed_text = zlib.compress(plaintext)
+        session_key = get_random_bytes(16)
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(compressed_text)
+
+        cipher_rsa, _ = self.get_rsa_cipher('other')
+        encrypted_session_key = cipher_rsa.encrypt(session_key)
+
+        msg_payload = encrypted_session_key +cipher_aes.nonce + tag + ciphertext
+        encrypted = base64.encodebytes(msg_payload)
+        return encrypted
+    
+    def sign_message(self, plaintext):
+        h = SHA256.new(plaintext)
+        signature = pkcs1_15.new(RSA.importKey(self.private_key)).sign(h)
+        return signature
+    
+    def decrypt_message(self, cipher):
+        encrypted_bytes = BytesIO(base64.decodebytes(cipher))
+        cipher_rsa, key_size_in_bytes = self.get_rsa_cipher('private')
+
+        encrypted_session_key = encrypted_bytes.read(key_size_in_bytes)
+        nonce = encrypted_bytes.read(16)
+        tag = encrypted_bytes.read(16)
+        ciphertext = encrypted_bytes.read()
+
+        session_key = cipher_rsa.decrypt(encrypted_session_key)
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+        decrypted = cipher_aes.decrypt_and_verify(ciphertext, tag)
+
+        plaintext = zlib.decompress(decrypted)
+
+        return plaintext
+    
+    def decrypt_signature(self, signature, plaintext):
+        h = SHA256.new(plaintext)
+        try:
+            pkcs1_15.new(RSA.importKey(self.other_pub_key)).verify(h, signature)
+            print("[*] Signature ok")
+            return True
+        except ValueError:
+            print("[!!] Signature not valid")
+            return False
+
+        
+
+
+
+
+
+    
---
+++ test-cipher-handler.py
@@ -0,0 +1,52 @@
+
+from Client.cipher import Cryptor
+
+number_test = 4
+class Test_Cipher:
+    def __init__(self):
+        self.cryptor2 = Cryptor('','','')
+        self.cryptor1 = Cryptor('','','')
+    
+    def test1(self):
+        print(f'[Tester] Test 1/{number_test} : generate keys')  
+        self.cryptor1.generate_keys()
+        self.cryptor2.generate_keys()
+        if(self.cryptor1.public_key != '' and self.cryptor1.public_key != '' and self.cryptor2.private_key != '' and self.cryptor2.public_key != ''):
+            print("[*] Test 1 ok")
+        else:
+            print("[!!] Test 1 ko")  
+
+    def test2(self):
+        print(f'[Tester] Test 2/{number_test} : set other key')
+        self.cryptor1.set_other_pub_key(self.cryptor2.public_key)
+        self.cryptor2.set_other_pub_key(self.cryptor1.public_key)
+        if(self.cryptor1.other_pub_key == self.cryptor2.public_key and self.cryptor2.other_pub_key == self.cryptor1.public_key):
+            print("[*] Test 2 ok")
+        else:
+            print("[!!] Test 2 ko")
+
+    def test3(self):
+        print(f'[Tester] Test 3/{number_test} encrypt message from A and decrypt from B')
+        message = b'Hello there'
+        ciphertext = self.cryptor1.encrypt_message(message)
+        decipher = self.cryptor2.decrypt_message(ciphertext)
+        if(decipher == message):
+            print("[*] Test 3 ok")
+        else:
+            print("[!!] Test 3 ko")
+
+    def test4(self):
+        print(f'[Tester] Test 4/{number_test} signature from A and check from B')
+        message = b'General Kenobi'
+        ciphertext = self.cryptor1.sign_message(message)
+        if(self.cryptor2.decrypt_signature(ciphertext,message)):
+            print("[*] Test 4 ok")
+        else:
+            print("[!!] Test 4 ko")
+
+if __name__ == '__main__':
+    test = Test_Cipher()
+    test.test1()
+    test.test2()
+    test.test3()
+    test.test4()
